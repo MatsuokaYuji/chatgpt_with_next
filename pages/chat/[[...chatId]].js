57076dce1cf1d2b196599c9df0a67f9729374360
松岡雄地 <matsuokayuuji@koji.local>
2024-01-13 01:25:19 +0900

    textform作成

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,6 +1,12 @@
 import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
-export default function Home() {
+import { useState } from "react";
+export default function ChatPage() {
+  const [messageText,setMessageText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("aaaa",messageText);
+  }
   return (
     <>
       <Head>
@@ -11,7 +17,19 @@ export default function Home() {
         {/* 子要素を垂直に配置する指示 */}
         <div className="flex flex-col bg-gray-700">
           <div className="flex-1">chat window</div>
-          <footer className="bg-gray-800 p-10">footer</footer>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                value={messageText}
+                onChange={e => setMessageText(e.target.value)}
+                 placeholder="Send a message..." className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"/>
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+            </footer>
         </div>
 
       </div>
